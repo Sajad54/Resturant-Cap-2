182545aae74a451ecd32b962505024c9eb4902e3
Sajad Babazadeh <sajadba@gmail.com>
2022-04-01 09:38:51 -0400

    formating phone number in reservation.js

--- front-end/src/reservations/Reservations.js
+++ front-end/src/reservations/Reservations.js
@@ -1,29 +1,26 @@
 import React from "react";
 import Reservation from "./Reservation";
-import formatReservationPhone from "./format-reservation-phone";
+import formatReservationPhone from "./utils/format-reservation-phone";
+
 export default function Reservations({ reservations }) {
-
-    const list = reservations.map(reservation => {
-        // TODO: 
-            // format phone number before we list it on the dashboard
-            formatReservationPhone(reservation.mobile_number);
-        console.log("line 10 Reservation.js", reservation.mobile_number)
-        return <Reservation 
-            key={reservation.reservation_id}
-            reservation_id={reservation.reservation_id}
-            first_name={reservation.first_name}
-            last_name={reservation.last_name}
-            mobile_number={reservation.mobile_number}
-            reservation_time={reservation.reservation_time}
-            people={reservation.people}
-            status={reservation.status}
-        />
-    });
-
-    return(
-        <div>
-            {list}
-        </div>
+  const list = reservations.map((reservation) => {
+    // TODO:
+    // format phone number before we list it on the dashboard
+    formatReservationPhone(reservation.mobile_number);
+    console.log("line 10 Reservation.js", reservation.mobile_number);
+    return (
+      <Reservation
+        key={reservation.reservation_id}
+        reservation_id={reservation.reservation_id}
+        first_name={reservation.first_name}
+        last_name={reservation.last_name}
+        mobile_number={reservation.mobile_number}
+        reservation_time={reservation.reservation_time}
+        people={reservation.people}
+        status={reservation.status}
+      />
     );
+  });
 
+  return <div>{list}</div>;
 }
