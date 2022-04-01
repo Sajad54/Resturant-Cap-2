4e11c7d7f43393b209e9a47b17360ef12d2966fc
Sajad Babazadeh <sajadba@gmail.com>
2022-03-31 20:42:46 -0400

    adding react variable in frontend api.js

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = "https://resturant-cap2.herokuapp.com/" || "http://localhost:5000";
+const REACT_APP_API_BASE_URL = "https://resturant-cap2.herokuapp.com" || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
